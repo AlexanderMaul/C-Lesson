deb495f2e98afeb39225d3added2031d84621fc6
Iskander <iskander-m.s@mail.ru>
2022-07-27 23:37:48 +0700

    Упражнение 11 выполнено

--- Example010_MethodArray/Program.cs
+++ Example010_MethodArray/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = { 13, 28, 143, 18, 84, 53, 602, 78, 18 };
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        // Для случаев когда моэет быть несколько ответов. а нужен всего один, можно
+        // воспользоваться прерыванием, как только будет найден первый
+        break;
+    }
+    // index = index + 1;
+    index++;
+}
+
+
